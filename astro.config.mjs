13a3fad57f03462aebe9007f76d03d0ad6e7a285
Don Macintosh <donmacintosh@Mac.mymodem>
2026-01-11 15:59:53 +1000

    Fix Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,16 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yourwillpro.com.au',
-  output: 'hybrid',
+  output: 'static',
   adapter: cloudflare(),
   integrations: [
     sitemap(),
   ],
 });
+```
+
+Save, then:
+```
+git add .
+git commit -m "Fix Astro config"
+git push
